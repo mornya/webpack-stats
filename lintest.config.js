c2851e5918de0a84d00e6a04fb338afd65f91e93
mornya <mornya78@gmail.com>
2023-11-03 02:33:35 +0900

    설정 파일 업데이트, Prettier 관련 설정 추가

--- lintest.config.js
+++ lintest.config.js
@@ -1,3 +1,8 @@
+/** @type {import('@lintest/core/lintest').Lintest.ConsumerConfig} */
 module.exports = {
   provider: 'mornya',
+  exportConfig: {
+    lint: '.eslintrc',
+    test: 'jest.config.json',
+  },
 };
